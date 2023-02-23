a56d4dab6cdf5368b4b4e8b74d8046fe8a46cfb0
ViktoriaTomchyshyn <viktoriia.tomchyshyn@gmail.com>
2023-02-23 17:24:20 +0300

    requests validation

--- ClothesShop/Catalog/Catalog.Host/Models/Requests/CreateItemRequest.cs
+++ ClothesShop/Catalog/Catalog.Host/Models/Requests/CreateItemRequest.cs
@@ -1,21 +1,34 @@
-﻿namespace Catalog.Host.Models.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.Host.Models.Requests
 {
     public class CreateItemRequest
     {
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
 
+        [StringLength(250)]
         public string Description { get; set; }
 
+        [Required]
+        [StringLength(30)]
         public string Category { get; set; }
 
+        [StringLength(30)]
         public string Brand { get; set; }
 
+        [Required]
+        [StringLength(10)]
         public string Size { get; set; }
 
+        [Required]
+        [Range(0, 99999.99)]
         public decimal Price { get; set; }
 
         public string PictureFileName { get; set; }
 
+        [Required]
         public int AvailableStock { get; set; }
 
     }
