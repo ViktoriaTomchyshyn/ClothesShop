c110a9b47d3c8f4eff23fd5d5be8783cb91fa09d
ViktoriaTomchyshyn <viktoriia.tomchyshyn@gmail.com>
2023-03-06 01:04:27 +0300

    ItemService tests

--- ClothesShop/Catalog/Catalog.UnitTests/Tests/ItemServiceTest.cs
+++ ClothesShop/Catalog/Catalog.UnitTests/Tests/ItemServiceTest.cs
@@ -8,6 +8,7 @@ using Catalog.Host.Services.Interfaces;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Logging;
 using Moq;
+using System;
 
 namespace Catalog.UnitTests.Tests
 {
@@ -52,43 +53,104 @@ namespace Catalog.UnitTests.Tests
         }
 
         [Fact]
-        public void GetAll_Success()
-        {
-        }
-
-        [Fact]
-        public void GetAll_Failed()
-        {
-        }
-
-        [Fact]
         public async Task AddItem_Success()
         {
+            //Arrange
+            var dto = new ItemDto
+            {
+                Name = _testItem.Name,
+                Description = _testItem.Description,
+                Category = _testItem.Category,
+                Brand = _testItem.Brand,
+                Size = _testItem.Size,
+                Price = _testItem.Price,
+                PictureUrl = _testItem.PictureFileName,
+                AvailableStock = _testItem.AvailableStock
+            };
+            _itemRepository.Setup(x => x.Add(dto.Name, dto.Description, dto.Category, dto.Brand, dto.Size, dto.Price, dto.PictureUrl, dto.AvailableStock)).ReturnsAsync(_testItem.Id);
+
+            //Act
+            var result = await _itemService.Add(dto.Name, dto.Description, dto.Category, dto.Brand, dto.Size, dto.Price, dto.PictureUrl, dto.AvailableStock);
+
+            //Assert
+            Assert.Equal(_testItem.Id, result);
         }
 
         [Fact]
         public async Task AddItem_Failed()
         {
+            //Arrange
+            var dto = new ItemDto
+            {
+                Name = _testItem.Name,
+                Description = _testItem.Description,
+                Category = _testItem.Category,
+                Brand = _testItem.Brand,
+                Size = _testItem.Size,
+                Price = _testItem.Price,
+                PictureUrl = _testItem.PictureFileName,
+                AvailableStock = _testItem.AvailableStock
+            };
+            _itemRepository.Setup(x => x.Add(dto.Name, dto.Description, dto.Category, dto.Brand, dto.Size, dto.Price, dto.PictureUrl, dto.AvailableStock)).ReturnsAsync(-1);
+
+            //Act
+            var result = await _itemService.Add(dto.Name, dto.Description, dto.Category, dto.Brand, dto.Size, dto.Price, dto.PictureUrl, dto.AvailableStock);
+
+            //Assert
+            Assert.NotEqual(_testItem.Id, result);
         }
 
         [Fact]
         public async Task DeleteItem_Success()
         {
+            // Arrange
+            _itemRepository.Setup(x => x.Delete(It.IsAny<int>())).ReturnsAsync(1);
+
+            // Act
+            var result = await _itemService.Delete(_testItem.Id);
+
+            // Assert
+            Assert.Equal(_testItem.Id, result);
+
         }
 
         [Fact]
         public async Task DeleteItem_Failed()
         {
+            // Arrange
+            _itemRepository.Setup(x => x.Delete(It.IsAny<int>())).ReturnsAsync((int?)null);
+
+            // Act
+            var result = await _itemService.Delete(_testItem.Id);
+
+            // Assert
+            Assert.Null(result);
         }
 
         [Fact]
         public async Task UpdateItem_Success()
         {
+            // Arrange
+            _itemRepository.Setup(x => x.Update(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<decimal>(), It.IsAny<string>(), It.IsAny<int>())).ReturnsAsync(1);
+
+            // Act
+            var result = await _itemService.Update(_testItem.Id, "newName", "newDescription", "newCategory", "newBrand", "newSize", 100, "newPictureFileName", 10);
+
+            // Assert
+            Assert.Equal(_testItem.Id, result);
         }
 
         [Fact]
         public async Task UpdateItem_Failed()
         {
+            // Arrange
+            _itemRepository.Setup(x => x.Update(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<decimal>(), It.IsAny<string>(), It.IsAny<int>())).ReturnsAsync((int?)null);
+
+            // Act
+            var result = await _itemService.Update(_testItem.Id, "newName", "newDescription", "newCategory", "newBrand", "newSize", 100, "newPictureFileName", 10);
+
+            // Assert
+            Assert.Null(result);
         }
 
     }
